6d333fd51ef35f6cf3629f4edb1d13f6c35fed42
Christian Febres <febres5cristian@hotmail.com>
2021-06-30 15:03:32 -0500

    Agregar funcionalidad boton google

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,17 @@ export const environment = {
   production: false
 };
 
+export const firebaseConfig ={
+  apiKey: "AIzaSyDcC10V2iUEaXKvYRRsRCZ2vp6T77rYSE0",
+  authDomain: "proyectopizza-a1591.firebaseapp.com",
+  projectId: "proyectopizza-a1591",
+  storageBucket: "proyectopizza-a1591.appspot.com",
+  messagingSenderId: "764116917836",
+  appId: "1:764116917836:web:01883e2ba046dea13c5ee9",
+  measurementId: "G-ZMQP0EMZN5"
+};
+
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
