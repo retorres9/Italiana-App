12090d9785ac1a24fac95f144d10eaa4c287177c
Christian Febres <febres5cristian@hotmail.com>
2021-07-06 17:47:48 -0500

    Obtener datos firebase

--- src/app/pizza/pizza.page.ts
+++ src/app/pizza/pizza.page.ts
@@ -2,38 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { ObtproductosService } from '../servicios/obtproductos.service';
 import { Producto } from './pizza.model';
 
-
-import { ActivatedRoute } from '@angular/router';
-
 @Component({
   selector: 'app-pizza',
   templateUrl: './pizza.page.html',
   styleUrls: ['./pizza.page.scss'],
 })
 export class PizzaPage implements OnInit {
-  products: Producto[]= [];
-  titulo: string;
-  constructor( private obtproductos: ObtproductosService,private activatedRoute: ActivatedRoute) { }
 
-  
-  
+products: Producto[]= [];
+
+  constructor( private obtproductos: ObtproductosService) { }
 
   ngOnInit() {
     this.obtproductos.productopizza.subscribe(
       resp=>{
         this.products = resp;
       }
-
     )
-    this.activatedRoute.params.subscribe(resp => {
-      console.log(resp);
-      this.titulo = resp.tipo;
-
-    })
   }
-
-  titulo: string;
-
 
   ionViewWillEnter(){
    this.obtproductos.getProducts().subscribe();
@@ -41,3 +27,4 @@ export class PizzaPage implements OnInit {
 
 
 }
+
